8d147714b2352761fb1c3989ebb4206f8c1e5348
quangtrungnguyen <trungpro5398@gmail.com>
2023-08-03 20:46:37 +1000

    Add Neighbour layout

--- carbon-footprint-calculation/src/components/Footprint/FootprintContainer.jsx
+++ carbon-footprint-calculation/src/components/Footprint/FootprintContainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import FootprintCard from "./FootprintCard";
-import PieChart from "../Chart/PieChart";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
 import "./FootprintContainer.scss";
 
 const FootprintContainer = () => {
+  const data = [
+    { name: "Gas", Consumption: 1000 },
+    { name: "Electricity", Consumption: 1000 },
+    { name: "CO2", Consumption: 1000 },
+  ];
+
   return (
     <div className="footprint-container">
       <div className="cards">
@@ -11,7 +25,14 @@ const FootprintContainer = () => {
         <FootprintCard type="Electricity" value="1000 kWh" />
         <FootprintCard type="CO2" value="1000 kWh" />
       </div>
-      <PieChart />
+      <BarChart width={500} height={300} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="Consumption" fill="#8884d8" />
+      </BarChart>
     </div>
   );
 };
